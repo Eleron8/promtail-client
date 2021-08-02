8aa1de8d371ac79f9e01ca6362bb92ff80689dd4
Eleron8 <eleron888@gmail.com>
2021-08-02 07:20:23 +0200

    added as module

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/Eleron8/promtail-client
+
+go 1.16
+
+require (
+	github.com/golang/protobuf v1.5.2
+	github.com/golang/snappy v0.0.4
+)
--- promtail/protoclient.go
+++ promtail/protoclient.go
@@ -2,13 +2,14 @@ package promtail
 
 import (
 	"fmt"
-	"github.com/golang/protobuf/proto"
-	"github.com/golang/protobuf/ptypes/timestamp"
-	"github.com/golang/snappy"
-	"github.com/afiskon/promtail-client/logproto"
 	"log"
 	"sync"
 	"time"
+
+	"github.com/Eleron8/promtail-client/logproto"
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/ptypes/timestamp"
+	"github.com/golang/snappy"
 )
 
 type protoLogEntry struct {
